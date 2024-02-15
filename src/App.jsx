b873ebff417f8609e460306f39cc3fd83ca23d50
Emon Dev180 <shuvroemon.cse@gmail.com>
2024-02-15 10:46:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+     <h1 className='text-5xl'>Emon dev</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h2 className="text-5xl text-center">Job Category list</h2>
+            <p className="text-center">Explor thousand of jobs opportunities with all the information </p>
+        </div>
+    );
+};
+
+export default CategoryList;
